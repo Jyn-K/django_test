646087ea49139cd54e2fde66f9e03095fd6b66f6
Jyn-K <y30308@gmail.com>
2025-11-14 06:58:01 +0000

    Replace: SQLite to MySQL

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        "OPTIONS": {
+            "read_default_file": "/etc/my.cnf",
+            #"read_default_file": BASE_DIR / "my.cnf",
+        },
     }
 }
 
